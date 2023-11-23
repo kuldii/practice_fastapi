a674f6245a7e7692f911f058b63eb381cc0573d5
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-11-23 23:58:33 +0500

    add image classification project

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
-# Practice FastAPI
+# Practice FastAPI Room Classification Project
+
+House & Apartaments Classification model
+
+Данная модель оперделяет какая комната в доме или квартире изображена на загруженном в нее изображении
+
+Примеры изображений:
+
+## Экстерьер
+![](/assets/example/exterior.jpeg)
+
+## Гардеробная
+
+![](/assets/example/closets.jpg)
+
+## Кухня
+
+![](/assets/example/kitchen.jpeg)
+
 
 ## Student Information
  * Sandikha Rahardi
  * РИМ-130908
 
+## Image Classification
+Toxic Comment Classification
+
+## Installation
+```bash
+pip install transformers
+```
+```bash
+pip install tensorflow
+```
+```bash
+pip install fastapi
+```
+```bash
+pip install uvicorn
+```
+```bash
+pip install python-multipart
+```
+
+## Running Project
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+import io
+import uvicorn
+from fastapi import File, UploadFile, Request, FastAPI
+from fastapi.templating import Jinja2Templates
+from PIL import Image
+from transformers import pipeline
+import tensorflow as tf
+    
+app = FastAPI()
+templates = Jinja2Templates(directory="assets")
+
+def load_model():
+    return pipeline(model="JuanMa360/room-classification")
+
+def preprocess_image(img):
+    img = img.resize((100, 100))
+    x = tf.keras.utils.img_to_array(img)
+    x = tf.keras.applications.efficientnet.preprocess_input(x)
+    return x
+
+@app.get("/")
+def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload")
+def upload(request: Request, file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open("assets/uploaded/" + file.filename, "wb") as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+        
+    loadedImage = Image.open(io.BytesIO(contents))
+
+    model = load_model()
+    
+    preprocess_image(loadedImage)
+    prediction = model.predict(loadedImage)
+    return {"result": prediction}
+
+if __name__ == "__main__":
+   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
